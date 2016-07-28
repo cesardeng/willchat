3e6023a39cf32c2601cf3aa51409136629ae176d
tianyong90 <412039588@qq.com>
2016-07-28 19:21:18 +0800

    gulp任务优化

--- gulpfile.js
+++ gulpfile.js
@@ -11,42 +11,71 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
+elixir(function (mix) {
     // mix.sass('app.scss');
 });
 
 var paths = {
-    js: ['public/js/user/!(*.min).js'],
-    css: ['public/css/user/!(*.min).css']
+    js: 'public/js',
+    css: 'public/css'
 };
 
 var gulp = require('gulp');
-var cssmin = require('gulp-minify-css');
+var fs = require('fs');
+var path = require('path');
+var merge = require('merge-stream');
+var minifycss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
-gulp.task('minifycss', function(){
-    gulp.src(paths.css)
-        .pipe(cssmin({
-            advanced: false,
-            compatibility: 'ie8',
-            keepBreaks: true
-        }))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('public/css/user'));
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+        .filter(function (file) {
+            return fs.statSync(path.join(dir, file)).isDirectory();
+        });
+}
+
+gulp.task('minifyjs', function () {
+    // public/js 下的了文件夹
+    var folders = getFolders(paths.js);
+
+    // 饮食上 public/js 目录本身
+    folders.push('/');
+
+    var tasks = folders.map(function (folder) {
+        return gulp.src(path.join(paths.js, folder, '/!(*.min).js'))
+            .pipe(uglify())
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest(path.join(paths.js, folder)));
+    });
+
+    return merge(tasks);
 });
 
-gulp.task('minifyjs', function(){
-    gulp.src(paths.js)
-        .pipe(uglify())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('public/js/user'));
+gulp.task('minifycss', function () {
+    // public/css 下的了文件夹
+    var folders = getFolders(paths.css);
+
+    // 饮食上 public/css 目录本身
+    folders.push('/');
+
+    var tasks = folders.map(function (folder) {
+        return gulp.src(path.join(paths.css, folder, '/!(*.min).css'))
+            .pipe(minifycss({
+                advanced: false,
+                compatibility: 'ie8',
+                keepBreaks: true
+            }))
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest(path.join(paths.css, folder)));
+    });
+
+    return merge(tasks);
 });
 
-gulp.task('watch', function() {
-    gulp.watch(paths.js, ['minifyjs']);
-    gulp.watch(paths.css, ['minifycss']);
+gulp.task('watch', function () {
+    gulp.watch(['public/js/!(*.min).js', 'public/js/user/!(*.min).js'], ['minifyjs']);
+    gulp.watch(['public/css/!(*.min).css', 'public/css/user/!(*.min).css'], ['minifycss']);
 });
 
 gulp.task('default', ['minifycss', 'minifyjs', 'watch']);
-
