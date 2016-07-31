d446b5b745c5bd9f15a65f82b84e0a2c5bf7022c
tianyong90 <412039588@qq.com>
2016-08-01 00:02:32 +0800

    模板调整

--- resources/views/user/layouts/base.blade.php
+++ resources/views/user/layouts/base.blade.php
@@ -16,21 +16,11 @@
   <link rel="stylesheet" href="{{ vendor('bower_components/AdminLTE') }}/dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="{{ vendor('bower_components/AdminLTE') }}/dist/css/skins/skin-black.min.css">
   <link rel="stylesheet" href="{{ vendor('bower_components/AdminLTE') }}/dist/css/skins/_all-skins.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="{{ vendor('bower_components/AdminLTE') }}/plugins/iCheck/flat/blue.css">
-  <!-- Morris chart -->
-  <link rel="stylesheet" href="{{ vendor('bower_components/AdminLTE') }}/plugins/morris/morris.css">
-  <!-- jvectormap -->
-  <link rel="stylesheet" href="{{ vendor('bower_components/AdminLTE') }}/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
-  <!-- Date Picker -->
-  <link rel="stylesheet" href="{{ vendor('bower_components/AdminLTE') }}/plugins/datepicker/datepicker3.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{ vendor('bower_components/AdminLTE') }}/plugins/daterangepicker/daterangepicker.css">
-  <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="{{ vendor('bower_components/AdminLTE') }}/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
-  <link rel="stylesheet" href="{{ css('simple-line-icons/simple-line-icons.min.css') }}">
 
+  <link rel="stylesheet" href="{{ css('simple-line-icons/simple-line-icons.min.css') }}">
+  <link rel="stylesheet" href="{{ css('user/common.min.css') }}">
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
@@ -51,7 +41,7 @@
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <section class="content-header">
+{{--     <section class="content-header">
       <h1>
         Dashboard
         <small>Control panel</small>
@@ -60,7 +50,7 @@
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
         <li class="active">Dashboard</li>
       </ol>
-    </section>
+    </section> --}}
 
     <!-- Main content -->
     <section class="content">
@@ -83,38 +73,13 @@
 
 <!-- jQuery 2.2.3 -->
 <script src="{{ vendor('bower_components/AdminLTE') }}/plugins/jQuery/jquery-2.2.3.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="http://cdn.bootcss.com/jqueryui/1.12.0/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button);
-</script>
 <!-- Bootstrap 3.3.6 -->
-<script src="{{ vendor('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-<!-- Morris.js charts -->
-<script src="http://cdn.bootcss.com/raphael/2.2.1/raphael.min.js"></script>
-<script src="{{ vendor('bower_components/AdminLTE') }}/plugins/morris/morris.min.js"></script>
-<!-- Sparkline -->
-<script src="{{ vendor('bower_components/AdminLTE') }}/plugins/sparkline/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="{{ vendor('bower_components/AdminLTE') }}/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="{{ vendor('bower_components/AdminLTE') }}/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="{{ vendor('bower_components/AdminLTE') }}/plugins/knob/jquery.knob.js"></script>
-<!-- daterangepicker -->
-<script src="https://cdn.bootcss.com/moment.js/2.14.1/moment.min.js"></script>
-<script src="{{ vendor('bower_components/AdminLTE') }}/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
-<script src="{{ vendor('bower_components/AdminLTE') }}/plugins/datepicker/bootstrap-datepicker.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="{{ vendor('bower_components/AdminLTE') }}/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
+<script src="{{ vendor('bower_components/AdminLTE') }}/bootstrap/js/bootstrap.min.js"></script>
+<!-- SlimScroll -->
 <script src="{{ vendor('bower_components/AdminLTE') }}/plugins/slimScroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
 <script src="{{ vendor('bower_components/AdminLTE') }}/plugins/fastclick/fastclick.js"></script>
 <!-- AdminLTE App -->
 <script src="{{ vendor('bower_components/AdminLTE') }}/dist/js/app.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ vendor('bower_components/AdminLTE') }}/dist/js/pages/dashboard.js"></script>
 </body>
 </html>
--- resources/views/user/layouts/pagesidebar.blade.php
+++ resources/views/user/layouts/pagesidebar.blade.php
@@ -2,7 +2,7 @@
   <!-- sidebar: style can be found in sidebar.less -->
   <section class="sidebar">
     <!-- Sidebar user panel -->
-{{--     <div class="user-panel">
+    <div class="user-panel">
       <div class="pull-left image">
         <img src="{{ $user->avatar }}" class="img-circle" alt="User Image">
       </div>
@@ -10,27 +10,24 @@
         <p>{{ $user->name }}</p>
         <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
       </div>
-    </div> --}}
+    </div>
     <!-- search form -->
-{{--     <form action="#" method="get" class="sidebar-form">
+    <form action="#" method="get" class="sidebar-form">
       <div class="input-group">
-        <input type="text" name="q" class="form-control" placeholder="Search...">
+        <input type="text" name="q" class="form-control" placeholder="搜索">
             <span class="input-group-btn">
               <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
               </button>
             </span>
       </div>
-    </form> --}}
+    </form>
     <!-- /.search form -->
     <!-- sidebar menu: : style can be found in sidebar.less -->
     <ul class="sidebar-menu">
       {{-- <li class="header">MAIN NAVIGATION</li> --}}
       <li class="active treeview">
-        <a href="#">
+        <a href="{{ user_url('/') }}">
           <i class="icon-home"></i> <span>管理主页</span>
-          <span class="pull-right-container">
-            <i class="fa fa-angle-left pull-right"></i>
-          </span>
         </a>
       </li>
       <li class="treeview">
